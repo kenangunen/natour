08b7443d2c1c91950a664d372ed8fce64ae5b760
kenangunen <gunenkenan@gmail.com>
2025-02-16 11:52:28 +0300

    feat: added unhandled and uncaughtException handler.

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+/**
+ * Eğer bir Promise zinciri .catch() içeriyorsa, bu hata unhandledRejection olarak algılanmaz.
+ * Burada mongoose.connect hatasını ele almış olduk.
+ */
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLER REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -9,6 +25,6 @@ const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSW
 mongoose.connect(DB, {}).then(() => console.log('DB connection succesful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
