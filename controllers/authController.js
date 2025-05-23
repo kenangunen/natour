3b9fc07adf583b88445fba88eb9ee66da28cfedb
kenangunen <gunenkenan@gmail.com>
2025-05-23 16:46:27 +0300

    feat: enhance JWT cookie handling in createSendToken function

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const sendEmail = require('./../utils/email');
+const jwt = require('jsonwebtoken');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -13,9 +14,28 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+  };
+
+  // Secure cookies are only sent over HTTPS connections
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true; // Set to true if using https
+  } else {
+    cookieOptions.secure = false; // Set to false if using http
+  }
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
+  user.passwordConfirm = undefined;
+
   res.status(statusCode).json({
     status: 'success',
-    token,
     data: {
       user: user,
     },
